77390127859782886f9bca6803d540a04d0dd84b
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-24 04:20:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,13 +11,4 @@ class Dog
     @name = new_name
   end
   
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
 end
